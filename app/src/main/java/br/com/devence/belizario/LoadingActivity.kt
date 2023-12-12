55866873840e807ea77386ece018fda13b36c2df
Thaian Ramalho <80857637+thaianramalho@users.noreply.github.com>
2023-12-12 10:33:12 -0300

    added loading

---
+++ app/src/main/java/br/com/devence/belizario/LoadingActivity.kt
@@ -0,0 +1,22 @@
+package br.com.devence.belizario
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+
+class LoadingActivity : AppCompatActivity() {
+
+    private val loadingTime: Long = 3000
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.loading_screen)
+
+        Handler().postDelayed({
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+            finish()
+        }, loadingTime)
+    }
+}
